53727290f64f4b0655ac1771b418601794183db1
Shiroshita <akihiro.shiroshita.1@vumc.org>
2022-11-04 09:31:09 -0500

    finalize assignment 2

--- cleaning/cleaning-NLHSRWD05G4DCP2.R
+++ cleaning/cleaning-NLHSRWD05G4DCP2.R
@@ -28,8 +28,6 @@ df1 <- read_csv("input/20221014AkiDataset/20221014AkiDataset_set1.csv")
 df2a <- read_csv("input/20221014AkiDataset/20221014AkiDataset_set2a.csv")
 df2b <- read_csv("input/20221014AkiDataset/20221014AkiDataset_set2b.csv")
 df3 <- read_csv("input/20221014AkiDataset/20221014AkiDataset_set3.csv")
-df4 <- read_csv("input/20221014AkiDataset/20221014AkiDataset_set4.csv")
-df5 <- read_csv("input/20221014AkiDataset/INSPIRE_RSV_Serology.csv")
 
 # Cleaning ----------------------------------------------------------------
 
@@ -41,20 +39,21 @@ df1 %>% glimpse()
 # making sex var (0: female, 1:male)
 # making race/ethnicity var (0: NHW, 1:NHB, 2:Hispanic and other)
 # making residence 6 month var (0: rural, 1:urban)
-# deleting completely missing residence_0y
+# deleting completely missing maternal education and residence_0y
 # delete state because all children are "TN"
 # change upper to lower case of CFSUBJD for merging to other datasets
 
 df1 <- df1 %>% 
-  dplyr::select(-MATSMOKE_6MOS, -MATSMOKE_OY, -OTHER_SMOKE_6MOS, -OTHER_SMOKE_OY) %>% 
+  select(-MATSMOKE_6MOS, -MATSMOKE_OY, -OTHER_SMOKE_6MOS, -OTHER_SMOKE_OY) %>% 
   mutate(sex = if_else(SEX == 2, 0, 1),
          ethnicity = case_when(RACE_WHT == 1 ~ 0,
                           RACE_BLK == 1 ~ 1,
                           RACE_AS == 1 | RACE_AIAN == 1 | RACE_MULT == 1 | RACE_OTH == 1 | HISPANIC == 1 | RACE_NHPI == 1 ~ 2),
          residence6mo = case_when(RESIDENCE_6MOS == "Urban" ~ 1,
                                   RESIDENCE_6MOS == "Rural" ~ 0,
-                                  RESIDENCE_6MOS == "Not found" ~ NA_real_)) %>% 
-  dplyr::select(-starts_with("RACE"), -SEX, -HISPANIC, -RESIDENCE_6MOS, -MAT_EDUC, -RESIDENCE_OY, -STATESITE) %>% 
+                                  RESIDENCE_6MOS == "Not found" ~ NA_real_),) %>% 
+  select(-starts_with("RACE"), -SEX, -HISPANIC, -RESIDENCE_6MOS) %>% 
+  select(-MAT_EDUC, -RESIDENCE_OY, -STATESITE) %>% 
   rename(cfsubjid = "CFSUBJID")
   
 ## cleaning of df2a ------
@@ -68,7 +67,7 @@ df2a <- df2a %>%
   group_by(cfsubjid) %>% 
   arrange(rftodaydate) %>%
   filter(row_number() == 1) %>% 
-  ungroup() 
+  ungroup()
 
 # join to df1
 
@@ -79,9 +78,12 @@ df <- left_join(df1, df2a, by = "cfsubjid")
 df2b %>% glimpse()
 
 # making serology var (0: negative, 1: positive)
+# delete completely missing oysmpbldrschdate
 
-df2b <- df2b %>%  
-  dplyr::select(-oysmpbldrschdate, -rsv_exposed_dev)
+df2b <- df2b %>% 
+  mutate(rsv_exposed_dev = case_when(rsv_exposed_dev == "no" ~ 0,
+                                     rsv_exposed_dev == "yes" ~ 1)) %>% 
+  select(oysmpbldrschdate)
 
 # join to df1
 
@@ -91,15 +93,12 @@ df <- left_join(df, df2b, by = "cfsubjid")
 
 df3 %>% glimpse()
 
-# some missing are coded as "missing"
-df3 <- df3 %>%
-  mutate(vycurrentasthma_dev = na_if(vycurrentasthma_dev, "missing"),
-         fycurrentasthma_dev = na_if(fycurrentasthma_dev, "missing"))
-
 # categorize to binary
 
 df3 <- df3 %>% 
-  mutate(oyrecwhz2p_dev = case_when(oyrecwhz2p_dev == "no" ~ 0,
+  mutate(fycurrentasthma_dev = case_when(fycurrentasthma_dev == "no" ~ 0,
+                                          fycurrentasthma_dev == "yes" ~ 1),
+         oyrecwhz2p_dev = case_when(oyrecwhz2p_dev == "no" ~ 0,
                                      oyrecwhz2p_dev == "yes" ~ 1),
          tyrecwhz2p_dev = case_when(tyrecwhz2p_dev == "no" ~ 0,
                                      tyrecwhz2p_dev == "yes" ~ 1),
@@ -112,38 +111,14 @@ df3 <- df3 %>%
          xyrecwhz2p_dev = case_when(xyrecwhz2p_dev == "no" ~ 0,
                                      xyrecwhz2p_dev == "yes" ~ 1),
          vycurrentasthma_dev = case_when(vycurrentasthma_dev == "no" ~ 0,
-                                         vycurrentasthma_dev == "yes" ~ 1),
-         fycurrentasthma_dev = case_when(fycurrentasthma_dev == "no" ~ 0,
-                                         fycurrentasthma_dev == "yes" ~ 1))
+                                         vycurrentasthma_dev == "yes" ~ 1)
+         )
 
 # join to df
 
 df <- left_join(df, df3, by = "cfsubjid")
 
-## cleaning of df4 ------
-# this sampling dataset
-df4 %>% glimpse()
-df4 <- df4 %>% 
-  arrange(lmsubjid, lmday1) %>% 
-  rename("cfsubjid" = "lmsubjid") %>% 
-  dplyr::select(cfsubjid, lmbarcode, lmday1) 
-
-## cleaning of serology dataset ------
-df5 %>% glimpse()
-df5 <- df5 %>% 
-  dplyr::select(lmsubjid, lmbarcode, rsv_exposed_dev) %>% 
-  rename("cfsubjid" = "lmsubjid") %>% 
-  mutate(rsv_exposed_dev = case_when(rsv_exposed_dev == "no" ~ 0,
-                                     rsv_exposed_dev == "yes" ~ 1),
-         # some missing are coded as "missing"
-         rsv_exposed_dev = na_if(rsv_exposed_dev, "missing"),
-         lmbarcode = str_sub(lmbarcode, end = -3))
-
-df45 <- left_join(df5, df4, by = c("cfsubjid", "lmbarcode"))
-
-## Cleaning
 # confirmation
 
-df <- left_join(df, df45, by = "cfsubjid")
 df %>% glimpse()
 df %>% write_csv("output/analysis_data.csv")
